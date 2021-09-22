30ac4f429730eef947574c1319478dd2b8838d2d
stnamco <lbnamco15@gmail.com>
2021-09-22 21:22:22 +0900

    init project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by kazuhiro nanko on 2021/09/22.
+//
+
+import UIKit
+import StructuredQRCodeReader
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        main()
+    }
+}
+
